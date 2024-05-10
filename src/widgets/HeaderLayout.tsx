168237f00e3137b0ac75b35dfe9cbf0eed2cbb2e
Reyney91 <andrew011901@mail.ru>
2024-05-11 00:26:29 +0500

    refactor: update layout

--- src/widgets/HeaderLayout.tsx
+++ src/widgets/HeaderLayout.tsx
@@ -1,17 +1,18 @@
 import { Box } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import { Suspense, useEffect } from 'react';
-import { useFavorites } from '@app/app/hooks';
+import { useAuth, useFavorites } from '@app/app/hooks';
 import { Header } from './Header';
 import { LoadingLayout } from './LoadingLayout';
 
 export const HeaderLayout = () => {
   const { setAllFavorites } = useFavorites();
+  const { isAuth } = useAuth();
   useEffect(() => {
-    setAllFavorites();
-    // Зависимости ругаются на то чтобы я добавил getAll внутрь, но тогда запустится вечный цикл
+    isAuth && setAllFavorites();
+    // Зависимости ругаются на то чтобы я добавил setAllFavorites внутрь, но тогда запустится вечный цикл
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [isAuth]);
 
   return (
     <>
